9fbdcab73ebbb3a0f74faf09bedf088541a930b5
Thomas Wack <twack@trek10.com>
2015-05-03 21:55:50 -0400

    Added a looping call as well as primitive calls for player movements

--- game.py
+++ game.py
@@ -5,13 +5,13 @@ import math
 import sys
 import pygame
 from pygame.locals import *
-
 from twisted.internet.protocol import Factory
 from twisted.internet.protocol import ClientFactory
 from twisted.internet.protocol import Protocol
-
 from twisted.internet import reactor
 from twisted.internet import protocol
+
+connections = dict()
 
 class GameSpace:
 	def main(self):
@@ -27,11 +27,22 @@ class GameSpace:
 		self.player2 = Player(self)
 		#enemies
 
-	def do_the_thing(self):	# 3) start game loop
+	# 3) start game loop
+	def do_the_thing(self):
 		print "doing the thing"
 		#while
-			#4) regulate tick speed
-			#5) handle user input events
+		#4) regulate tick speed is done in the game class
+		
+		#5) handle user input events
+		for event in pygame.event.get():
+			if event.type == KEYDOWN and is_client = 0:
+				self.player1.move(event.key)
+				connections['GAME'].transport.write(str(event.key))
+			elif event.type == KEYDOWN and is_client = 1:
+				self.player2.move(event.key)
+				connections['GAME'].transport.write(str(event.key))
+			elif event.type == QUIT:
+				reactor.stop()
 			#6) ongoing behavior, send everything a tick
 			#7) animations
 
@@ -40,21 +51,22 @@ class Game(Protocol):
 		self.users = users
 		self.player_number = is_client + 1
 	
-
 	def connectionMade(self):
-		message = "hello"
-		self.transport.write(message)
-		print "CONNECTION MADE"
+		#message = "hello"
+		#self.transport.write(message)
+		#print "CONNECTION MADE"
 		#print is_client
 		print "You are Player " + str(self.player_number)
-		#if is_client == 0:
-			#go send infor
-		#if is_client == 1:
-			#otherwise you wait
-
+		connections['GAME'] = self
+		lc = LoopingCall(gs.do_the_thing())
+        lc.start(1/60)
 
 	def dataReceived(self, data):
-		print data
+		#print data
+		if is_client == 0:
+			gs.player1.move(int(data))
+		else:
+			gs.player2.move(int(data))
 
 	def connectionLost(self,reason):
 		print "Connection lost - goodbye!"
@@ -70,10 +82,8 @@ class GameFactory(ClientFactory):
 	def buildProtocol(self, addr):
 		return Game(self.users)
 
-
-
 if __name__ == "__main__":
-	#gs = GameSpace()
+	gs = GameSpace()
 	if sys.argv[1] == "host":
 		reactor.listenTCP(8000, GameFactory())
 		is_client = 0
