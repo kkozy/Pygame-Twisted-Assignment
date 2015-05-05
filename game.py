5a5263be5386d219cbde1326e151d9665ecbbb1f
Kyle Kozak <kkozak@nd.edu>
2015-05-04 20:56:21 -0400

    Game update and sprites
    Player Sprites added
    More game functionality:
    - Working game display
    - some player movement

--- game.py
+++ game.py
@@ -10,63 +10,139 @@ from twisted.internet.protocol import ClientFactory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
 from twisted.internet import protocol
+from twisted.internet import task
+from twisted.internet.task import LoopingCall
+import pickle
+#data = pickle.dumps(data)
 
 connections = dict()
+is_client = sys.argv[1]
+player_positions = dict()
+
+class Player1(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+		self.gs = gs
+		self.image = pygame.image.load("p1.png")
+		self.image = pygame.transform.scale(self.image,(50,30))
+		self.rect = self.image.get_rect()
+		self.orig_image = self.image	# keep original to limit resize errors
+		self.rect = self.rect.move(500, 420)
+
+	def tick(self):
+		if is_client == "host":
+			#gets mouse x and y positions
+			mx, my = pygame.mouse.get_pos()
+			px = self.rect.centerx
+			py = self.rect.centery
+			self.angle = (0) - math.degrees(math.atan2(my-py, mx-px))
+			self.image = pygame.transform.rotate(self.orig_image, self.angle)
+			self.rect = self.image.get_rect(center = (self.rect.centerx, self.rect.centery) )
+		return
+
+	def move(self, keycode):
+		if keycode == K_RIGHT and self.rect.x <= 600:
+			self.rect = self.rect.move(2, 0)
+		if keycode == K_LEFT and self.rect.x >= 0:
+			self.rect = self.rect.move(-2, 0)
+		if keycode == K_UP and self.rect.y >= 360:
+			self.rect = self.rect.move(0, -2)
+		if keycode == K_DOWN and self.rect.y <= 440:
+			self.rect = self.rect.move(0, 2)
+		return
+
+class Player2(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+		self.gs = gs
+		self.image = pygame.image.load("p2.png")
+		self.image = pygame.transform.scale(self.image,(30,50))
+		self.rect = self.image.get_rect()
+		self.orig_image = self.image	# keep original to limit resize errors
+		self.rect = self.rect.move(180, 420)
+
+	def tick(self):
+		if is_client == "client":
+			#gets mouse x and y positions
+			mx, my = pygame.mouse.get_pos()
+			px = self.rect.centerx
+			py = self.rect.centery
+			self.angle = (-90) - math.degrees(math.atan2(my-py, mx-px))
+			self.image = pygame.transform.rotate(self.orig_image, self.angle)
+			self.rect = self.image.get_rect(center = (self.rect.centerx, self.rect.centery) )
+		return
+
+	def move(self, keycode):
+		if keycode == K_RIGHT and self.rect.x <= 600:
+			self.rect = self.rect.move(2, 0)
+		if keycode == K_LEFT and self.rect.x >= 0:
+			self.rect = self.rect.move(-2, 0)
+		if keycode == K_UP and self.rect.y >= 360:
+			self.rect = self.rect.move(0, -2)
+		if keycode == K_DOWN and self.rect.y <= 440:
+			self.rect = self.rect.move(0, 2)
+		return
 
 class GameSpace:
 	def main(self):
 		#1) basic initialization
 		pygame.init()
+		pygame.key.set_repeat(1,1)
 		self.size = self.width, self.height = 640, 480
 		self.black = 0, 0, 0
 		self.screen = pygame.display.set_mode(self.size)
 		
 		# 2) set up game objects
 		self.clock = pygame.time.Clock()
-		self.player1 = Player(self)
-		self.player2 = Player(self)
+
+		self.is_client = is_client
+
+		self.player1 = Player1(self)
+		self.player2 = Player2(self)
 		#enemies
 
 	# 3) start game loop
 	def do_the_thing(self):
-		print "doing the thing"
+		#print "doing the thing"
 		#while
 		#4) regulate tick speed is done in the game class
 		
 		#5) handle user input events
 		for event in pygame.event.get():
-			if event.type == KEYDOWN and is_client = 0:
+			if event.type == KEYDOWN and is_client == "host":
 				self.player1.move(event.key)
 				connections['GAME'].transport.write(str(event.key))
-			elif event.type == KEYDOWN and is_client = 1:
+			elif event.type == KEYDOWN and is_client == "client":
 				self.player2.move(event.key)
 				connections['GAME'].transport.write(str(event.key))
-			elif event.type == QUIT:
+
+			if event.type == pygame.QUIT:
 				reactor.stop()
-			#6) ongoing behavior, send everything a tick
-			#7) animations
+				sys.exit()
+
+		#6) ongoing behavior, send everything a tick
+		self.player1.tick()
+		self.player2.tick()
+		#7) animations
+		#player_posittions
+		self.screen.fill(self.black)
+		self.screen.blit(self.player1.image, self.player1.rect)
+		self.screen.blit(self.player2.image, self.player2.rect)
+		pygame.display.flip()
 
 class Game(Protocol):
 	def __init__(self, users):
 		self.users = users
-		self.player_number = is_client + 1
-	
+		
 	def connectionMade(self):
-		#message = "hello"
-		#self.transport.write(message)
-		#print "CONNECTION MADE"
-		#print is_client
-		print "You are Player " + str(self.player_number)
 		connections['GAME'] = self
-		lc = LoopingCall(gs.do_the_thing())
-        lc.start(1/60)
 
 	def dataReceived(self, data):
 		#print data
-		if is_client == 0:
-			gs.player1.move(int(data))
-		else:
+		if is_client == "host":
 			gs.player2.move(int(data))
+		else:
+			gs.player1.move(int(data))
 
 	def connectionLost(self,reason):
 		print "Connection lost - goodbye!"
@@ -82,13 +158,19 @@ class GameFactory(ClientFactory):
 	def buildProtocol(self, addr):
 		return Game(self.users)
 
+
+
 if __name__ == "__main__":
 	gs = GameSpace()
+	gs.main()
+
 	if sys.argv[1] == "host":
+		lc = task.LoopingCall(gs.do_the_thing)
+		lc.start(0.02)
 		reactor.listenTCP(8000, GameFactory())
-		is_client = 0
 		reactor.run() 
 	elif sys.argv[1] == "client":
+		lc = task.LoopingCall(gs.do_the_thing)
+		lc.start(0.02)
 		reactor.connectTCP('localhost', 8000, GameFactory())
-		is_client = 1
 		reactor.run() 
